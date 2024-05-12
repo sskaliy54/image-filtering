b301f903daf8b2d2d5b717ad183217e5fa6cd3c4
Tetiana Skalii <tetiana.skalii.ki.2020@lpnu.ua>
2024-05-12 21:30:46 +0300

    Finished samples page

---
+++ ViewModel/HelpPageVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using STimg.Models;
+
+namespace STimg.ViewModel
+{
+    class HelpPageVM
+    {
+       // private readonly PageModel _pageModel;
+    }
+}
---
+++ ViewModel/NavigationVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using STimg.Helpers;
+using System.Windows.Input;
+using STimg.View;
+using prob.Properties;
+
+namespace STimg.ViewModel
+{
+    class NavigationVM : BaseVM
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand EditCommand { get; set; }
+        public ICommand HelpCommand { get; set; }
+        public ICommand SamplesCommand { get; set; }
+
+
+        private void Edit(object obj) => CurrentView = new EditPageVM();
+        private void Help (object obj) => CurrentView = new HelpPageVM();
+        private void Samples(object obj) => CurrentView = new SamplesPageVM();
+
+
+        public NavigationVM()
+        {
+            EditCommand = new RelayCommand(Edit);
+            HelpCommand = new RelayCommand(Help);
+            SamplesCommand = new RelayCommand(Samples);
+             
+
+            // Startup Page
+            CurrentView = new EditPageVM();
+        }
+    }
+}
