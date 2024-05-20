4bdc0acd70f162890f2123f153a8c3a773a90296
Tetiana Skalii <tetiana.skalii.ki.2020@lpnu.ua>
2024-05-20 15:31:35 +0300

    Added services

--- ViewModel/EditPageVM.cs
+++ ViewModel/EditPageVM.cs
@@ -14,16 +14,23 @@ using Emgu.CV.Util;
 using Emgu.CV.DnnSuperres;
 using Emgu.CV.Structure;
 using BitmapExtension = STimg.Extension.BitmapExtension;
-
+using STimg.Enums;
+using STimg.Services;
 
 
 namespace STimg.ViewModel
 {
-    public class EditPageVM : BaseVM
+    public class EditPageVM : BaseVM 
     {
+        private const double MaxNoiseLevel = 70;
+        private const double MaxBlurLevel = 100;
+        private const double MinContrast = 50;
+        private const double MaxAvgColorValue = 100;
+
         private BitmapImage _uploadedImage;
         private RelayCommand _applyFSRCNNCommand;
 
+        private FilterImageService filterImageService = new FilterImageService();
         public RelayCommand UploadCommand { get; private set; }
         public RelayCommand ApplyBoxFilterCommand { get; private set; }
         public RelayCommand ApplyGaussianBlurCommand { get; private set; }
@@ -81,39 +88,6 @@ namespace STimg.ViewModel
             AnalyzeAndSuggestCommand = new RelayCommand(AnalyzeAndSuggest);
         }
 
-        public BitmapImage MatToBitmapImage(Mat imageMat)
-        {
-            using (var stream = new MemoryStream())
-            {
-                imageMat.ToBitmap().Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
-                stream.Position = 0;
-                BitmapImage bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.StreamSource = stream;
-                bitmapImage.EndInit();
-                bitmapImage.Freeze();
-                return bitmapImage;
-            }
-        }
-
-        public enum FilterType
-        {
-            Box,
-            GaussianBlur,
-            Gray,
-            Warm,
-            DetailEnhancing,
-            Cold,
-            Retro,
-            Bilateral,
-            Pink,
-            NoiseReduction,
-            ContrastEnhancement,
-            BrightnessAdjustment,
-            WhiteBalance,
-            BlurReduction
-        }
         public void ApplyFilter(FilterType filterType, Mat imageMat = null)
         {
             if (UploadedImage == null) return;
@@ -136,8 +110,8 @@ namespace STimg.ViewModel
                     {
                         channels = new VectorOfMat();
                         CvInvoke.Split(imageMat, channels);
-                        ApplyLookupTable(channels[2], BuildLookupTable(0.95));
-                        ApplyLookupTable(channels[0], BuildLookupTable(1.1));
+                        filterImageService.ApplyLookupTable(channels[2], filterImageService.BuildLookupTable(0.95));
+                        filterImageService.ApplyLookupTable(channels[0], filterImageService.BuildLookupTable(1.1));
                         CvInvoke.Merge(channels, imageMat);
                     }
                     break;
@@ -152,8 +126,8 @@ namespace STimg.ViewModel
                     {
                         channels = new VectorOfMat();
                         CvInvoke.Split(imageMat, channels);
-                        ApplyLookupTable(channels[2], BuildLookupTable(1.25));
-                        ApplyLookupTable(channels[0], BuildLookupTable(0.95));
+                        filterImageService.ApplyLookupTable(channels[2], filterImageService.BuildLookupTable(1.25));
+                        filterImageService.ApplyLookupTable(channels[0], filterImageService.BuildLookupTable(0.95));
                         CvInvoke.Merge(channels, imageMat);
                     }
                     break;
@@ -162,10 +136,10 @@ namespace STimg.ViewModel
                     {
                         channels = new VectorOfMat();
                         CvInvoke.Split(imageMat, channels);
-                        ApplyLookupTable(channels[2], BuildLookupTable(0.95));
-                        ApplyLookupTable(channels[1], BuildLookupTable(0.8));
-                        ApplyLookupTable(channels[0], BuildLookupTable(0.95));
-                        ApplyGammaCorrection(channels, 0.65);
+                        filterImageService.ApplyLookupTable(channels[2], filterImageService.BuildLookupTable(0.95));
+                        filterImageService.ApplyLookupTable(channels[1], filterImageService.BuildLookupTable(0.8));
+                        filterImageService.ApplyLookupTable(channels[0], filterImageService.BuildLookupTable(0.95));
+                        filterImageService.ApplyGammaCorrection(channels, 0.65);
                         CvInvoke.Merge(channels, imageMat);
                         CvInvoke.BoxFilter(imageMat, imageMat, DepthType.Cv8U, new System.Drawing.Size(3, 3), new System.Drawing.Point(-1, -1));
                     }
@@ -183,10 +157,10 @@ namespace STimg.ViewModel
                     {
                         channels = new VectorOfMat();
                         CvInvoke.Split(imageMat, channels);
-                        ApplyLookupTable(channels[2], BuildLookupTable(1.4));
-                        ApplyLookupTable(channels[1], BuildLookupTable(1.4));
-                        ApplyLookupTable(channels[0], BuildLookupTable(1.2));
-                        ApplyGammaCorrection(channels, 0.95);
+                        filterImageService.ApplyLookupTable(channels[2], filterImageService.BuildLookupTable(1.4));
+                        filterImageService.ApplyLookupTable(channels[1], filterImageService.BuildLookupTable(1.4));
+                        filterImageService.ApplyLookupTable(channels[0], filterImageService.BuildLookupTable(1.2));
+                        filterImageService.ApplyGammaCorrection(channels, 0.95);
                         CvInvoke.Merge(channels, imageMat);
                         CvInvoke.BoxFilter(imageMat, imageMat, DepthType.Cv8U, new System.Drawing.Size(3, 3), new System.Drawing.Point(-1, -1));
                     }
@@ -214,7 +188,7 @@ namespace STimg.ViewModel
                 case FilterType.BrightnessAdjustment:
                     channels = new VectorOfMat();
                     CvInvoke.Split(imageMat, channels);
-                    ApplyGammaCorrection(channels, 0.45);
+                    filterImageService.ApplyGammaCorrection(channels, 0.45);
                     break;
                 case FilterType.WhiteBalance:
                     if (!isGrayImage)
@@ -235,9 +209,9 @@ namespace STimg.ViewModel
                     }
                     break;
                 case FilterType.BlurReduction:
-                    double variance = GetBlurLevel(imageMat);
+                    double variance = filterImageService.GetBlurLevel(imageMat);
                     int scaleIncreaseFactor = (int)(variance / 100) + 1;
-                    Mat kernel = CreateSharpeningKernel();
+                    Mat kernel = filterImageService.CreateSharpeningKernel();
                     for (int i = 0; i < scaleIncreaseFactor; i++)
                     {
                         CvInvoke.Filter2D(imageMat, imageMat, kernel, new System.Drawing.Point(-1, -1));
@@ -246,7 +220,7 @@ namespace STimg.ViewModel
 
             }
 
-            UploadedImage = MatToBitmapImage(imageMat);
+            UploadedImage = filterImageService.MatToBitmapImage(imageMat);
         }
 
         public void ApplyFSRCNN()
@@ -254,7 +228,7 @@ namespace STimg.ViewModel
             if (UploadedImage == null) return;
 
             Mat imageMat = STimg.Extension.BitmapExtension.ToMat(UploadedImage);
-            double variance = ComputeLaplacianVariance(imageMat);
+            double variance = filterImageService.ComputeLaplacianVariance(imageMat);
             const double blurThreshold = 100.0;
 
             if (variance < blurThreshold)
@@ -264,7 +238,7 @@ namespace STimg.ViewModel
 
                 int scaleIncreaseFactor = (int)(variance / blurThreshold) + 1;
                 int enhanceSteps = 3 * scaleIncreaseFactor;
-                Mat kernel = CreateSharpeningKernel();
+                Mat kernel = filterImageService.CreateSharpeningKernel();
                 for (int i = 0; i < enhanceSteps; i++)
                 {
                     CvInvoke.Filter2D(denoisedImage, imageMat, kernel, new System.Drawing.Point(-1, -1));
@@ -292,10 +266,9 @@ namespace STimg.ViewModel
                 Mat convertedImage = new Mat();
                 CvInvoke.CvtColor(imageMat, convertedImage, ColorConversion.Bgra2Bgr);
                 fsrcnn.Upsample(convertedImage, result);
-                UploadedImage = MatToBitmapImage(result);
+                UploadedImage = filterImageService.MatToBitmapImage(result);
             }
         }
-
         public void AnalyzeAndSuggest(object obj)
         {
             if (UploadedImage == null) return;
@@ -309,21 +282,16 @@ namespace STimg.ViewModel
             }
         }
 
-        private const double MaxNoiseLevel = 70;
-        private const double MaxBlurLevel = 100;
-        private const double MinContrast = 50;
-        private const double MaxAvgColorValue = 100;
         private List<FilterType> AnalyzeAndSuggestFilters(Mat imageMat)
         {
-            double[] avgColor = GetAverageColor(imageMat);
-            double brightness = GetBrightness(imageMat);
-            double contrast = GetContrast(imageMat);
-            double noiseLevel = GetNoiseLevel(imageMat);
-            double blurLevel = GetBlurLevel(imageMat);
-            bool hasHighlights = HasHighlights(imageMat);
-            bool hasShadows = HasShadows(imageMat);
-            bool whiteBalanceIssue = HasWhiteBalanceIssue(avgColor);
-
+            double[] avgColor = filterImageService.GetAverageColor(imageMat);
+            double brightness = filterImageService.GetBrightness(imageMat);
+            double contrast = filterImageService.GetContrast(imageMat);
+            double noiseLevel = filterImageService.GetNoiseLevel(imageMat);
+            double blurLevel = filterImageService.GetBlurLevel(imageMat);
+            bool hasHighlights = filterImageService.HasHighlights(imageMat);
+            bool hasShadows = filterImageService.HasShadows(imageMat);
+            bool whiteBalanceIssue = filterImageService.HasWhiteBalanceIssue(avgColor);
 
             List<FilterType> suggestedFilters = new List<FilterType>();
 
@@ -408,38 +376,6 @@ namespace STimg.ViewModel
                 }
             }
         }
-
-        private Mat CreateSharpeningKernel()
-        {
-            float[] kernelValues = { -1, -1, -1, -1, 9, -1, -1, -1, -1 };
-            Mat kernel = new Mat(3, 3, DepthType.Cv32F, 1);
-            kernel.SetTo(kernelValues);
-            return kernel;
-        }
-
-        private double ComputeLaplacianVariance(Mat image)
-        {
-            Mat gray = new Mat();
-            if (image.NumberOfChannels == 3)
-            {
-                CvInvoke.CvtColor(image, gray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
-            }
-            else
-            {
-                gray = image.Clone();
-            }
-
-            Mat laplacian = new Mat();
-            CvInvoke.Laplacian(gray, laplacian, Emgu.CV.CvEnum.DepthType.Cv64F);
-
-            MCvScalar mean = new MCvScalar();
-            MCvScalar stddev = new MCvScalar();
-            CvInvoke.MeanStdDev(laplacian, ref mean, ref stddev);
-
-            double variance = stddev.V0;
-            return variance * variance;
-        }
-
         private void ApplyMultipleFilters(object obj)
         {
             if (UploadedImage == null) return;
@@ -451,179 +387,8 @@ namespace STimg.ViewModel
             {
                 ApplyFilter(filter, imageMat);
             }
-
-            UploadedImage = MatToBitmapImage(imageMat);
-        }
-        private byte[] BuildLookupTable(double gamma)
-        {
-            byte[] lookupTable = new byte[256];
-            for (int i = 0; i < 256; i++)
-            {
-                lookupTable[i] = (byte)Math.Min(255, (int)(Math.Pow(i / 255.0, gamma) * 255.0));
-            }
-            return lookupTable;
-        }
-        private Mat BuildGammaLut(double gamma)
-        {
-            byte[] lut = new byte[256];
-            for (int i = 0; i < 256; i++)
-            {
-                lut[i] = (byte)Math.Min(255, Math.Pow(i / 255.0, 1 / gamma) * 255);
-            }
-            Mat lutMat = new Mat(1, 256, DepthType.Cv8U, 1);
-            lutMat.SetTo(lut);
-            return lutMat;
-        }
-        private void ApplyLookupTable(Mat channel, byte[] lookupTable)
-        {
-            Mat lookup = new Mat(256, 1, DepthType.Cv8U, 1);
-            System.Runtime.InteropServices.Marshal.Copy(lookupTable, 0, lookup.DataPointer, lookupTable.Length);
-            CvInvoke.LUT(channel, lookup, channel);
-        }
-        private void ApplyGammaCorrection(VectorOfMat channels, double gamma)
-        {
-            Mat lut = BuildGammaLut(gamma);
-            for (int i = 0; i < channels.Size; i++)
-            {
-                CvInvoke.LUT(channels[i], lut, channels[i]);
-            }
+            UploadedImage = filterImageService.MatToBitmapImage(imageMat);
         }
 
-        private double[] GetAverageColor(Mat img)
-        {
-            Mat bgr = new Mat();
-            if (img.NumberOfChannels == 1)
-            {
-                CvInvoke.CvtColor(img, bgr, ColorConversion.Gray2Bgr);
-            }
-            else
-            {
-                bgr = img;
-            }
-
-            Mat hsv = new Mat();
-            CvInvoke.CvtColor(bgr, hsv, ColorConversion.Bgr2Hsv);
-            MCvScalar avgHsv = CvInvoke.Mean(hsv);
-            return new double[] { avgHsv.V0, avgHsv.V1, avgHsv.V2 };
-        }
-
-        private double GetBrightness(Mat img)
-        {
-            Mat gray = new Mat();
-            if (img.NumberOfChannels == 3)
-            {
-                CvInvoke.CvtColor(img, gray, ColorConversion.Bgr2Gray);
-            }
-            else
-            {
-                gray = img;
-            }
-
-            MCvScalar avgBrightness = CvInvoke.Mean(gray);
-            return avgBrightness.V0;
-        }
-        private double GetContrast(Mat img)
-        {
-            Mat gray = new Mat();
-            if (img.NumberOfChannels == 3)
-            {
-                CvInvoke.CvtColor(img, gray, ColorConversion.Bgr2Gray);
-            }
-            else
-            {
-                gray = img;
-            }
-
-            MCvScalar mean = new MCvScalar();
-            MCvScalar stddev = new MCvScalar();
-            CvInvoke.MeanStdDev(gray, ref mean, ref stddev);
-            return stddev.V0;
-        }
-
-        private double GetNoiseLevel(Mat img)
-        {
-            Mat bgr = new Mat();
-            if (img.NumberOfChannels == 1)
-            {
-                CvInvoke.CvtColor(img, bgr, ColorConversion.Gray2Bgr);
-            }
-            else
-            {
-                bgr = img;
-            }
-            Mat floatImage = new Mat();
-            bgr.ConvertTo(floatImage, DepthType.Cv32F);
-            Mat mean = new Mat();
-            Mat stddev = new Mat();
-            CvInvoke.MeanStdDev(floatImage, mean, stddev);
-            double[] stddevValues = new double[1];
-            stddev.CopyTo(stddevValues);
-            double noiseLevel = stddevValues[0];
-            return noiseLevel;
-        }
-
-        private double GetBlurLevel(Mat img)
-        {
-            Mat gray = new Mat();
-            if (img.NumberOfChannels == 3)
-            {
-                CvInvoke.CvtColor(img, gray, ColorConversion.Bgr2Gray);
-            }
-            else
-            {
-                gray = img;
-            }
-
-            Mat laplacian = new Mat();
-            CvInvoke.Laplacian(gray, laplacian, DepthType.Cv64F);
-
-            MCvScalar mean = new MCvScalar();
-            MCvScalar stddev = new MCvScalar();
-            CvInvoke.MeanStdDev(laplacian, ref mean, ref stddev);
-
-            double variance = stddev.V0;
-            return variance * variance;
-        }
-
-        private bool HasHighlights(Mat img)
-        {
-            Mat gray = new Mat();
-            if (img.NumberOfChannels == 1)
-            {
-                CvInvoke.CvtColor(img, img, ColorConversion.Gray2Bgr);
-            }
-            CvInvoke.CvtColor(img, gray, ColorConversion.Bgr2Gray);
-
-            double highlightThreshold = 240;
-            Mat highlightMask = new Mat();
-            CvInvoke.Threshold(gray, highlightMask, highlightThreshold, 255, ThresholdType.Binary);
-            int highlightCount = CvInvoke.CountNonZero(highlightMask);
-            return highlightCount > (gray.Rows * gray.Cols * 0.05);
-        }
-
-        private bool HasShadows(Mat img)
-        {
-            Mat gray = new Mat();
-            if (img.NumberOfChannels == 1)
-            {
-                CvInvoke.CvtColor(img, img, ColorConversion.Gray2Bgr);
-            }
-            CvInvoke.CvtColor(img, gray, ColorConversion.Bgr2Gray);
-            double shadowThreshold = 20;
-            Mat shadowMask = new Mat();
-            CvInvoke.Threshold(gray, shadowMask, shadowThreshold, 255, ThresholdType.BinaryInv);
-            int shadowCount = CvInvoke.CountNonZero(shadowMask);
-            return shadowCount > (gray.Rows * gray.Cols * 0.05);
-        }
-
-        private bool HasWhiteBalanceIssue(double[] avgColor)
-        {
-            double maxDiff = 100;
-            return Math.Abs(avgColor[0] - avgColor[1]) > maxDiff ||
-                   Math.Abs(avgColor[0] - avgColor[2]) > maxDiff ||
-                   Math.Abs(avgColor[1] - avgColor[2]) > maxDiff;
-        }
-
-    }
-    
+    } 
 }
